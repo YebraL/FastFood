c59dae510b9818d9700526ed289a350ee241fa27
LuigiY <Product4conker@Gmail.com>
2022-12-05 14:37:32 -0600

    scafold

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+// import './App.css'
+import axios from 'axios'
+
+function App() {
+  
+  
+
+  return (
+    <div className="App">
+      <h1>Start Here</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ client/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // vite uses this as a prefix for href and src URLs
+  base: '/static/',
+  build: {
+    //where vite puts the bundled application
+    outDir:'../server/static',
+
+    // delete old build when creating new
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+  plugins: [react()]
+})
