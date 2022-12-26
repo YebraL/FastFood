ebd79acfd9ea72bfbea22ae250be16243883546a
LuigiY <Product4conker@Gmail.com>
2022-12-26 14:03:47 -0600

    Added HomePage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
 // import './App.css'
 import axios from 'axios'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/home';
 
 function App() {
   
 
   return (
     <div className="App">
+    
       <h1>Start Here</h1>
+      <Home/>
     </div>
   )
 }
