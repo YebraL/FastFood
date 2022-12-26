25828f4b135e161d00e96d7ab98c5b964ba11f8c
LuigiY <Product4conker@Gmail.com>
2022-12-26 15:14:12 -0600

    Added LoginPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
 // import './App.css'
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {HashRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Home from './pages/home';
+import Login from './pages/Login';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   
@@ -10,8 +15,35 @@ function App() {
   return (
     <div className="App">
     
-      <h1>Start Here</h1>
-      <Home/>
+      <h1>Food Court</h1>
+      <div>
+      
+        <Router>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        
+                        <Nav.Item>
+                          <Link to='/'> Home </Link>
+                        </Nav.Item>
+                        
+                        <Nav.Item>
+                          <Link to='login'> Login </Link>
+                        </Nav.Item>
+
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='login/' element={<Login/>}/>
+            </Routes>
+        </Router>
+      </div>
+      
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 function Home() {
+
+
   return (
-    <div>home</div>
+    <div>
+        
+        <h4>Home</h4>
+
+    </div>
+
+    
   )
 }
 
